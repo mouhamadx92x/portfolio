4c1964eda8761251ac3b149bcdc1203ea27ad333
mouhamad <mouhamadx92x@gmail.com>
2024-02-11 19:05:56 +0100

    styles the header

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -4,60 +4,52 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import {
+    headerAppBarStyles,
+    headerToolbarStyles,
+    headerLinkTypographyStyles,
+    headerLinkStyles,
+    headerMainBoxStyles,
+} from "./styles";
 
 const Header = () => {
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar
-                position="sticky"
-                sx={{
-                    width: "50vw",
-                    margin: "0 auto",
-                    backgroundColor: "transparent",
-                    border: "0",
-                    boxShadow: "none",
-                }}
-            >
-                <Toolbar
-                    sx={{
-                        display: "flex",
-                        justifyContent: "space-between",
-                        alignItems: "center",
-                    }}
-                >
-                    <Link
-                        href="/home"
-                        passHref
-                        style={{ textDecoration: "none" }}
-                    >
-                        <Typography variant="body1" component="a">
+        <Box sx={headerMainBoxStyles}>
+            <AppBar position="sticky" sx={headerAppBarStyles}>
+                <Toolbar sx={headerToolbarStyles}>
+                    <Link href="/home" passHref style={headerLinkStyles}>
+                        <Typography
+                            variant="body1"
+                            component="a"
+                            sx={headerLinkTypographyStyles}
+                        >
                             Home
                         </Typography>
                     </Link>
-                    <Link
-                        href="/projects"
-                        passHref
-                        style={{ textDecoration: "none" }}
-                    >
-                        <Typography variant="body1" component="a">
+                    <Link href="/projects" passHref style={headerLinkStyles}>
+                        <Typography
+                            variant="body1"
+                            component="a"
+                            sx={headerLinkTypographyStyles}
+                        >
                             Projects
                         </Typography>
                     </Link>
-                    <Link
-                        href="/cv"
-                        passHref
-                        style={{ textDecoration: "none" }}
-                    >
-                        <Typography variant="body1" component="a">
+                    <Link href="/cv" passHref style={headerLinkStyles}>
+                        <Typography
+                            variant="body1"
+                            component="a"
+                            sx={headerLinkTypographyStyles}
+                        >
                             CV
                         </Typography>
                     </Link>
-                    <Link
-                        href="/contact"
-                        passHref
-                        style={{ textDecoration: "none" }}
-                    >
-                        <Typography variant="body1" component="a">
+                    <Link href="/contact" passHref style={headerLinkStyles}>
+                        <Typography
+                            variant="body1"
+                            component="a"
+                            sx={headerLinkTypographyStyles}
+                        >
                             Contact Me
                         </Typography>
                     </Link>
