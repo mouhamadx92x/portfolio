62ed566780d9e6ec49353ef72c3afb98d4f64f15
mouhamad <mouhamadx92x@gmail.com>
2024-02-13 00:09:22 +0100

    styles

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -4,25 +4,58 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 
 const Footer = () => {
     return (
-        <Box>
-            <Box>
+        <Box
+            sx={{
+                height: "35vh",
+                backgroundColor: "#000",
+                color: "#949494",
+                display: "flex",
+                justifyContent: "space-between",
+                padding: "30px",
+            }}
+        >
+            <Box
+                sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "flex-start",
+                }}
+            >
                 <Typography>TO CONTACT:</Typography>
-                <Box>
-                    <Box>
+                <Box
+                    sx={{
+                        display: "flex",
+                        marginTop: "20px",
+                        marginBottom: "20px",
+                    }}
+                >
+                    <Box sx={{ display: "flex", flexDirection: "column" }}>
                         <Typography>Tel: +49 (0) 176 248 22011</Typography>
                         <Typography>E-Mail: mouhamadx92x@gmail.com</Typography>
                     </Box>
-                    <Box>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            flexDirection: "column",
+                            marginLeft: "30px",
+                        }}
+                    >
                         <Typography>Am Vierrutenberg 4A</Typography>
                         <Typography> 13055 Berlin</Typography>
                     </Box>
                 </Box>
-                <Box>
-                    <Link href="https://www.linkedin.com/in/mohammad-alsbinati-466369262/">
-                        <LinkedInIcon fontSize="medium" />
+                <Box sx={{ marginBottom: "20px" }}>
+                    <Link href="https://www.linkedin.com/in/your-profile">
+                        <LinkedInIcon
+                            fontSize="medium"
+                            sx={{ color: "#949494" }}
+                        />
                     </Link>
-                    <Link href="https://github.com/mouhamadx92x">
-                        <GitHubIcon fontSize="medium" />
+                    <Link href="https://github.com/your-username">
+                        <GitHubIcon
+                            fontSize="medium"
+                            sx={{ color: "#949494", marginLeft: "30px" }}
+                        />
                     </Link>
                 </Box>
                 <Typography>
