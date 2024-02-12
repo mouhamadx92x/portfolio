9f9a36c2ce53077b46a6f8aefe286c31c94ab0c8
mouhamad <mouhamadx92x@gmail.com>
2024-02-13 00:31:25 +0100

    refactoring

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -1,60 +1,42 @@
 import { Box, Link, Typography } from "@mui/material";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import {
+    footerMainBoxStyles,
+    FooterContactInfoBoxStyles,
+    footerContactDetailsBoxStyles,
+    footerAddressDetailsBoxStyle,
+    footerPersonalContactDetailsBoxStyle,
+    footerSocialMediaBoxStyle,
+    footerSocialMediaStyle,
+} from "./styles";
 
 const Footer = () => {
     return (
-        <Box
-            sx={{
-                height: "35vh",
-                backgroundColor: "#000",
-                color: "#949494",
-                display: "flex",
-                justifyContent: "space-between",
-                padding: "30px",
-            }}
-        >
-            <Box
-                sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "flex-start",
-                }}
-            >
+        <Box sx={footerMainBoxStyles}>
+            <Box sx={FooterContactInfoBoxStyles}>
                 <Typography>TO CONTACT:</Typography>
-                <Box
-                    sx={{
-                        display: "flex",
-                        marginTop: "20px",
-                        marginBottom: "20px",
-                    }}
-                >
-                    <Box sx={{ display: "flex", flexDirection: "column" }}>
+                <Box sx={footerContactDetailsBoxStyles}>
+                    <Box sx={footerPersonalContactDetailsBoxStyle}>
                         <Typography>Tel: +49 (0) 176 248 22011</Typography>
                         <Typography>E-Mail: mouhamadx92x@gmail.com</Typography>
                     </Box>
-                    <Box
-                        sx={{
-                            display: "flex",
-                            flexDirection: "column",
-                            marginLeft: "30px",
-                        }}
-                    >
+                    <Box sx={footerAddressDetailsBoxStyle}>
                         <Typography>Am Vierrutenberg 4A</Typography>
                         <Typography> 13055 Berlin</Typography>
                     </Box>
                 </Box>
-                <Box sx={{ marginBottom: "20px" }}>
+                <Box sx={footerSocialMediaBoxStyle}>
                     <Link href="https://www.linkedin.com/in/your-profile">
                         <LinkedInIcon
                             fontSize="medium"
-                            sx={{ color: "#949494" }}
+                            sx={footerSocialMediaStyle}
                         />
                     </Link>
                     <Link href="https://github.com/your-username">
                         <GitHubIcon
                             fontSize="medium"
-                            sx={{ color: "#949494", marginLeft: "30px" }}
+                            sx={footerSocialMediaStyle}
                         />
                     </Link>
                 </Box>
---
+++ src/app/components/footer/styles.tsx
@@ -0,0 +1,39 @@
+export const footerMainBoxStyles = {
+    height: "35vh",
+    backgroundColor: "#000",
+    color: "#949494",
+    display: "flex",
+    justifyContent: "space-between",
+    padding: "30px",
+};
+
+export const FooterContactInfoBoxStyles = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+};
+
+export const footerContactDetailsBoxStyles = {
+    display: "flex",
+    marginTop: "20px",
+    marginBottom: "20px",
+};
+
+export const footerAddressDetailsBoxStyle = {
+    display: "flex",
+    flexDirection: "column",
+    marginLeft: "30px",
+};
+
+export const footerPersonalContactDetailsBoxStyle = {
+    display: "flex",
+    flexDirection: "column",
+};
+
+export const footerSocialMediaBoxStyle = {
+    marginBottom: "20px",
+    display: "flex",
+    justifyContent: "space-evenly",
+    width: "150px",
+};
+export const footerSocialMediaStyle = { color: "#949494" };
