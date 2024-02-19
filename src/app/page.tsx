0d29e8d87adc63327a645649e4c691aee1a2490a
mouhamad <mouhamadx92x@gmail.com>
2024-02-19 12:02:41 +0100

    styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,14 @@ import Footer from "./components/footer";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useEffect, useState } from "react";
+import { homePageMainBoxStyles, homePageProjectsCardBoxStyles } from "./styles";
 
 const HomePage = () => {
     const [boxHeight, setBoxHeight] = useState("500px");
-    const [secondBoxHeight, setSecondBoxHeight] = useState("500px");
     useEffect(() => {
         const handleScroll = () => {
             const newHeight = Math.max(300, 500 - window.scrollY / 2) + "px";
             setBoxHeight(newHeight);
-            const newSecondBoxHeight =
-                Math.min(800, 500 + window.scrollY / 2) + "px";
-            setSecondBoxHeight(newSecondBoxHeight);
         };
 
         window.addEventListener("scroll", handleScroll);
@@ -27,20 +24,10 @@ const HomePage = () => {
 
     return (
         <>
-            <Box
-                sx={{
-                    background: "url(/images/background.jpg)",
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                    backgroundRepeat: "no-repeat",
-                    height: boxHeight,
-                }}
-            >
+            <Box sx={{ ...homePageMainBoxStyles, height: boxHeight }}>
                 <Header />
             </Box>
-            <Box
-                sx={{ height: secondBoxHeight, backgroundColor: "#2b2b2b" }}
-            ></Box>
+            <Box sx={homePageProjectsCardBoxStyles}></Box>
             <Footer />
             <ToastContainer position="bottom-center" limit={1} />
         </>
---
+++ src/app/styles.tsx
@@ -0,0 +1,14 @@
+export const homePageMainBoxStyles = {
+    background: "url(/images/background.jpg)",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+};
+
+export const homePageProjectsCardBoxStyles = {
+    backgroundColor: "#2b2b2b",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "start",
+};
