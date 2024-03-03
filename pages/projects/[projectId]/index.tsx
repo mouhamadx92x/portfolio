b21853a32afd997f62bf165c879882a3fab61c0d
mouhamad <mouhamadx92x@gmail.com>
2024-03-03 10:36:39 +0100

    adding project details

--- pages/projects/[projectId]/index.tsx
+++ pages/projects/[projectId]/index.tsx
@@ -1,10 +1,24 @@
-import { Box } from "@mui/material";
+import { Box, Link, Typography } from "@mui/material";
 import CarouselWithStepper from "./carousel-with-stepper/index";
 
 const ProjectDetails = () => {
     return (
         <Box>
             <CarouselWithStepper />
+            <Typography>Name</Typography>
+            <Typography>description:</Typography>
+            <Typography>Feature:</Typography>
+            <Box>
+                <Link href="#" underline="none">
+                    see the live project
+                </Link>
+                <Link href="#" underline="none">
+                    Github
+                </Link>
+                <Link href="#" underline="none">
+                    Back to Projects
+                </Link>
+            </Box>
         </Box>
     );
 };
