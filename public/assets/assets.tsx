5ffc8918573b6423aa5945feb4d6130fc7e7bd8c
mouhamad <mouhamadx92x@gmail.com>
2024-03-06 19:32:28 +0100

    Images path

--- public/assets/assets.tsx
+++ public/assets/assets.tsx
@@ -1,4 +1,19 @@
 import {
+    PROJECT_TODO_IMAGE_PATH,
+    PROJECT_TODO_IMAGE_PATH_01,
+    PROJECT_TODO_IMAGE_PATH_02,
+    PROJECT_TODO_IMAGE_PATH_03,
+    PROJECT_WEATHER_IMAGE_PATH,
+    PROJECT_WEATHER_IMAGE_PATH_01,
+    PROJECT_WEATHER_IMAGE_PATH_02,
+    PROJECT_WEATHER_IMAGE_PATH_03,
+    PROJECT_WEATHER_IMAGE_PATH_04,
+    PROJECT_VIDEOS_STUDIO_IMAGE_PATH,
+    PROJECT_VIDEOS_STUDIO_IMAGE_PATH_01,
+    PROJECT_VIDEOS_STUDIO_IMAGE_PATH_02,
+    PROJECT_VIDEOS_STUDIO_IMAGE_PATH_03,
+} from "../constants/paths";
+import {
     PROJECT_NAME_TODO,
     PROJECT_NAME_WEATHER,
     PROJECT_NAME_VIDEOS_STUDIO,
@@ -11,7 +26,12 @@ export const projectsData = [
         projectNumber: "0 1",
         description:
             "This application is a simple yet powerful To-Do list manager built with React. It allows users to add, delete, and mark tasks as completed. The interface is user-friendly, making it easy to manage your tasks efficiently.",
-        imgPath: "/images/To-do.png",
+        imagesPath: [
+            PROJECT_TODO_IMAGE_PATH,
+            PROJECT_TODO_IMAGE_PATH_01,
+            PROJECT_TODO_IMAGE_PATH_02,
+            PROJECT_TODO_IMAGE_PATH_03,
+        ],
         webSiteLink: "https://todo-app-mou.netlify.app/",
         creationDate: "8/23",
     },
@@ -21,7 +41,13 @@ export const projectsData = [
         projectNumber: "0 2",
         description:
             "This application is a user-friendly Weather Forecast tool built with React. It allows users to search for the current weather conditions of any city around the world. The app leverages a reliable weather API to fetch real-time weather data, providing users with accurate and up-to-date information.",
-        imgPath: "/images/Weather.png",
+        imagesPath: [
+            PROJECT_WEATHER_IMAGE_PATH,
+            PROJECT_WEATHER_IMAGE_PATH_01,
+            PROJECT_WEATHER_IMAGE_PATH_02,
+            PROJECT_WEATHER_IMAGE_PATH_03,
+            PROJECT_WEATHER_IMAGE_PATH_04,
+        ],
         webSiteLink: "https://weather-app-mou.netlify.app/",
         creationDate: "9/23",
     },
@@ -31,7 +57,12 @@ export const projectsData = [
         projectNumber: "0 3",
         description:
             "Videos Studio is a frontend Project, was a collaborative effort, developed in partnership with Ameer Alaswad. We created a Interior Designer's Projects Gallery.",
-        imgPath: "/images/Videos-studio.png",
+        imagesPath: [
+            PROJECT_VIDEOS_STUDIO_IMAGE_PATH,
+            PROJECT_VIDEOS_STUDIO_IMAGE_PATH_01,
+            PROJECT_VIDEOS_STUDIO_IMAGE_PATH_02,
+            PROJECT_VIDEOS_STUDIO_IMAGE_PATH_03,
+        ],
         webSiteLink: "https://videos-gallery.netlify.app/",
         creationDate: "1/24",
     },
--- public/constants/paths.tsx
+++ public/constants/paths.tsx
@@ -5,3 +5,32 @@ export const PROJECTS_PATH = "/projects";
 export const CV_PATH = "/cv";
 
 export const CONTACT_ME_PATH = "/contact";
+
+export const PROJECT_TODO_IMAGE_PATH = "/images/To-do.png";
+
+export const PROJECT_TODO_IMAGE_PATH_01 = "/images/To-do-01.png";
+
+export const PROJECT_TODO_IMAGE_PATH_02 = "/images/To-do-02.png";
+
+export const PROJECT_TODO_IMAGE_PATH_03 = "/images/To-do-03.png";
+
+export const PROJECT_WEATHER_IMAGE_PATH = "/images/Weather.png";
+
+export const PROJECT_WEATHER_IMAGE_PATH_01 = "/images/Weather-01.png";
+
+export const PROJECT_WEATHER_IMAGE_PATH_02 = "/images/Weather-02.png";
+
+export const PROJECT_WEATHER_IMAGE_PATH_03 = "/images/Weather-03.png";
+
+export const PROJECT_WEATHER_IMAGE_PATH_04 = "/images/Weather-04.png";
+
+export const PROJECT_VIDEOS_STUDIO_IMAGE_PATH = "/images/Videos-studio.png";
+
+export const PROJECT_VIDEOS_STUDIO_IMAGE_PATH_01 =
+    "/images/Videos-studio-01.png";
+
+export const PROJECT_VIDEOS_STUDIO_IMAGE_PATH_02 =
+    "/images/Videos-studio-02.png";
+
+export const PROJECT_VIDEOS_STUDIO_IMAGE_PATH_03 =
+    "/images/Videos-studio-03.png";
