bda26a8f3de864fccaa8598f249d3dbcb2dc883e
mouhamad <mouhamadx92x@gmail.com>
2024-03-10 11:48:16 +0100

    type the variables

--- pages/projects/[projectId]/index.tsx
+++ pages/projects/[projectId]/index.tsx
@@ -15,10 +15,13 @@ import { useRouter } from "next/router";
 
 const ProjectDetails = () => {
     const router = useRouter();
-    const { description, feature, webSiteLink, githubLink, name, imagesPath } =
-        router.query;
-    console.log(typeof description);
-    console.log(name);
+
+    const description = router.query.description as string;
+    const feature = router.query.feature as string[];
+    const webSiteLink = router.query.webSiteLink as string;
+    const githubLink = router.query.githubLink as string;
+    const name = router.query.name as string;
+    const imagesPath = router.query.imagesPath as string[];
 
     return (
         <>
