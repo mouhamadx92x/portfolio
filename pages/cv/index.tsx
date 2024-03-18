dbac714de4018796538d1d9dcfc47a1d6ddc779d
mouhamad <mouhamadx92x@gmail.com>
2024-03-18 23:18:14 +0100

    styles

--- pages/cv/index.tsx
+++ pages/cv/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import { Box, Button } from "@mui/material";
 import FileSaver from "file-saver";
+import "../../src/styles/global.css";
 
 const ImageWithDownload: React.FC = () => {
     const imageSrc = "/images/cv.png";
@@ -26,18 +27,54 @@ const ImageWithDownload: React.FC = () => {
             .then((blob) => {
                 FileSaver.saveAs(blob, pdfName);
             })
-            .catch((error) => console.error("Error downloading image:", error));
+            .catch((error) => console.error("Error downloading PDF:", error));
     };
 
     return (
-        <Box>
-            <Image src={imageSrc} alt={imageAlt} width={800} height={1000} />
-            <Button variant="contained" color="primary" onClick={downloadImage}>
-                Download image
-            </Button>
-            <Button variant="contained" color="primary" onClick={downloadPdf}>
-                Download pdf
-            </Button>
+        <Box
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                backgroundColor: "#2b2b2b",
+                padding: "20px",
+            }}
+        >
+            <Box
+                sx={{
+                    marginBottom: 2,
+                    display: "flex",
+                    alignContent: "space-between",
+                    justifyContent: "space-between",
+                    width: "500px",
+                }}
+            >
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={downloadImage}
+                    sx={{ color: "#f2f2f2", backgroundColor: "#857b5f" }}
+                >
+                    Download image
+                </Button>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={downloadPdf}
+                >
+                    Download pdf
+                </Button>
+            </Box>
+            <Image
+                src={imageSrc}
+                alt={imageAlt}
+                width={800}
+                height={1000}
+                style={{
+                    boxShadow:
+                        "0  4px  8px  0 rgba(149,  123,  95,  0.2),  0  6px  20px  0 rgba(149,  123,  95,  0.19)",
+                }}
+            />
         </Box>
     );
 };
