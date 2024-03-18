c9198b8f619698cefec7d664fa6c84271bc784f9
mouhamad <mouhamadx92x@gmail.com>
2024-03-18 17:22:14 +0100

    install file saver and create the component

---
+++ pages/cv/index.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Image from "next/image";
+import { Box, Button } from "@mui/material";
+import FileSaver from "file-saver";
+
+const ImageWithDownload: React.FC = () => {
+    const imageSrc = "/images/cv.png";
+    const imageName = "cv.png";
+    const imageAlt = "cv image";
+
+    const pdfSrc = "/cv/CV.pdf";
+    const pdfName = "CV.pdf";
+
+    const downloadImage = () => {
+        fetch(imageSrc)
+            .then((response) => response.blob())
+            .then((blob) => {
+                FileSaver.saveAs(blob, imageName);
+            })
+            .catch((error) => console.error("Error downloading image:", error));
+    };
+
+    const downloadPdf = () => {
+        fetch(pdfSrc)
+            .then((response) => response.blob())
+            .then((blob) => {
+                FileSaver.saveAs(blob, pdfName);
+            })
+            .catch((error) => console.error("Error downloading image:", error));
+    };
+
+    return (
+        <Box>
+            <Image src={imageSrc} alt={imageAlt} width={800} height={1000} />
+            <Button variant="contained" color="primary" onClick={downloadImage}>
+                Download image
+            </Button>
+            <Button variant="contained" color="primary" onClick={downloadPdf}>
+                Download pdf
+            </Button>
+        </Box>
+    );
+};
+
+export default ImageWithDownload;
