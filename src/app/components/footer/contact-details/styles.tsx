7497046967a7f047465e7a2002701d88e295a57f
mouhamad <mouhamadx92x@gmail.com>
2024-03-24 23:02:50 +0100

    footer contact details RD

--- src/app/components/footer/contact-details/styles.tsx
+++ src/app/components/footer/contact-details/styles.tsx
@@ -4,32 +4,38 @@ export const footerContactInfoBoxStyles = {
     display: "flex",
     flexDirection: "column",
     alignItems: "flex-start",
-    justifyContent: "space-around",
-    width: { xs: "350px", md: "400px" },
+    justifyContent: "space-between",
+    width: { xs: "350px", md: "450px" },
     height: "330px",
     color: BLEACHED_SILK_COLOR,
+    marginBottom: { xs: "10px" },
+    marginTop: { xs: "10px" },
 };
 
 export const footerContactDetailsBoxStyles = {
     display: "flex",
+    flexDirection: { xs: "column", md: "row" },
+    justifyContent: "space-between",
+    width: { xs: "350px", md: "450px" },
+    marginBottom: { xs: "10px" },
+    marginTop: { xs: "10px" },
 };
 
 export const footerAddressDetailsBoxStyle = {
     display: "flex",
     flexDirection: "column",
-    marginLeft: "20px",
-    marginRight: "15px",
+    marginBottom: { xs: "10px" },
 };
 
 export const footerPersonalContactDetailsBoxStyle = {
-    display: "flex",
-    flexDirection: "column",
+    marginBottom: { xs: "10px" },
 };
 
 export const footerSocialMediaBoxStyle = {
     display: "flex",
     justifyContent: "start",
     width: "150px",
+    marginBottom: { xs: "10px" },
 };
 export const footerSocialMediaStyle = {
     color: BLEACHED_SILK_COLOR,
