e7c9d6de3f7cf6e309df1ebd68f0ae155e8f61db
mouhamad <mouhamadx92x@gmail.com>
2024-03-24 14:05:43 +0100

    modifying MUI break point

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,38 +9,53 @@ import ProductCard from "./components/project-card";
 import { projectsData } from "../../public/assets/assets";
 import "../styles/global.css";
 import useScrollHeight from "@/hooks/useScrollHeight";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 const HomePage = () => {
     const boxHeight = useScrollHeight({ initialValue: 500 });
 
+    const theme = createTheme({
+        breakpoints: {
+            values: {
+                xs: 0,
+                sm: 760,
+                md: 900,
+                lg: 1200,
+                xl: 1536,
+            },
+        },
+    });
+
     return (
         <>
-            <Box sx={{ ...homePageMainBoxStyles, height: boxHeight }}>
-                {/* <Header /> */}
-            </Box>
-            <Box sx={homePageProjectsCardBoxStyles}>
-                {projectsData.map(
-                    ({
-                        imagesPath,
-                        name,
-                        creationDate,
-                        webSiteLink,
-                        description,
-                        id,
-                    }) => (
-                        <ProductCard
-                            key={id}
-                            imagesPath={imagesPath}
-                            name={name}
-                            creationDate={creationDate}
-                            webSiteLink={webSiteLink}
-                            description={description}
-                        />
-                    )
-                )}
-            </Box>
-            {/* <Footer /> */}
-            <ToastContainer position="bottom-center" limit={1} />
+            <ThemeProvider theme={theme}>
+                <Box sx={{ ...homePageMainBoxStyles, height: boxHeight }}>
+                    {/* <Header /> */}
+                </Box>
+                <Box sx={homePageProjectsCardBoxStyles}>
+                    {projectsData.map(
+                        ({
+                            imagesPath,
+                            name,
+                            creationDate,
+                            webSiteLink,
+                            description,
+                            id,
+                        }) => (
+                            <ProductCard
+                                key={id}
+                                imagesPath={imagesPath}
+                                name={name}
+                                creationDate={creationDate}
+                                webSiteLink={webSiteLink}
+                                description={description}
+                            />
+                        )
+                    )}
+                </Box>
+                <Footer />
+                <ToastContainer position="bottom-center" limit={1} />
+            </ThemeProvider>
         </>
     );
 };
