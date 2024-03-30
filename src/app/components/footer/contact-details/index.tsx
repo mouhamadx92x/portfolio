c1d98692c0b4d6484ec3014ecce306a580d714fb
mouhamad <mouhamadx92x@gmail.com>
2024-03-30 02:43:52 +0100

    footer contact info resize

--- src/app/components/footer/contact-details/index.tsx
+++ src/app/components/footer/contact-details/index.tsx
@@ -4,8 +4,9 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import {
     footerContactInfoBoxStyles,
     footerContactDetailsBoxStyles,
-    footerSocialMediaBoxStyle,
-    footerSocialMediaStyle,
+    footerSocialMediaBoxStyles,
+    footerSocialMediaStyles,
+    FooterContactInfoTextStyles,
 } from "./styles";
 import {
     FOOTER_CONTACT_ADDRESS_SECTION_LABEL,
@@ -19,29 +20,44 @@ import {
 const FooterContactDetails = () => {
     return (
         <Box sx={footerContactInfoBoxStyles}>
-            <Typography>{FOOTER_CONTACT_ADDRESS_SECTION_LABEL}</Typography>
+            <Typography sx={FooterContactInfoTextStyles}>
+                {FOOTER_CONTACT_ADDRESS_SECTION_LABEL}
+            </Typography>
             <Box sx={footerContactDetailsBoxStyles}>
                 <Box>
-                    <Typography>{FOOTER_CONTACT_MOBIL_NUMBER}</Typography>
-                    <Typography>{FOOTER_CONTACT_EMAIL}</Typography>
+                    <Typography sx={FooterContactInfoTextStyles}>
+                        {FOOTER_CONTACT_MOBIL_NUMBER}
+                    </Typography>
+                    <Typography sx={FooterContactInfoTextStyles}>
+                        {FOOTER_CONTACT_EMAIL}
+                    </Typography>
                 </Box>
                 <Box>
-                    <Typography>{FOOTER_CONTACT_ADDRESS_STREET}</Typography>
-                    <Typography>{FOOTER_CONTACT_ADDRESS_POST_CODE}</Typography>
+                    <Typography sx={FooterContactInfoTextStyles}>
+                        {FOOTER_CONTACT_ADDRESS_STREET}
+                    </Typography>
+                    <Typography sx={FooterContactInfoTextStyles}>
+                        {FOOTER_CONTACT_ADDRESS_POST_CODE}
+                    </Typography>
                 </Box>
             </Box>
-            <Box sx={footerSocialMediaBoxStyle}>
+            <Box sx={footerSocialMediaBoxStyles}>
                 <Link href="https://www.linkedin.com/in/your-profile">
                     <LinkedInIcon
                         fontSize="medium"
-                        sx={footerSocialMediaStyle}
+                        sx={footerSocialMediaStyles}
                     />
                 </Link>
                 <Link href="https://github.com/your-username">
-                    <GitHubIcon fontSize="medium" sx={footerSocialMediaStyle} />
+                    <GitHubIcon
+                        fontSize="medium"
+                        sx={footerSocialMediaStyles}
+                    />
                 </Link>
             </Box>
-            <Typography>{FOOTER_CONTACT_COPY_RIGHT}</Typography>
+            <Typography sx={FooterContactInfoTextStyles}>
+                {FOOTER_CONTACT_COPY_RIGHT}
+            </Typography>
         </Box>
     );
 };
--- src/app/components/footer/contact-details/styles.tsx
+++ src/app/components/footer/contact-details/styles.tsx
@@ -2,31 +2,34 @@ import { BLEACHED_SILK_COLOR } from "../../../../../public/constants/colors";
 
 export const footerContactInfoBoxStyles = {
     width: { xs: "350px", sm: "250px", md: "450px" },
-    height: "300px",
+    height: { xs: "225px", sm: "300px", md: "225px" },
     color: BLEACHED_SILK_COLOR,
     display: "flex",
     flexDirection: "column",
     justifyContent: "space-between",
+    fontSize: "10px",
 };
 
 export const footerContactDetailsBoxStyles = {
     display: "flex",
-    height: { xs: "125px", sm: "150px", md: "75px" },
+    height: { xs: "105px", sm: "125px", md: "75px" },
     flexDirection: { xs: "column", sm: "column", md: "row" },
     justifyContent: {
-        xs: "space-around",
-        sm: "space-around",
-        md: "space-around",
+        xs: "space-between",
+        sm: "space-between",
+        md: "space-between",
     },
 };
 
-export const footerSocialMediaBoxStyle = {
+export const FooterContactInfoTextStyles = { fontSize: "16px" };
+
+export const footerSocialMediaBoxStyles = {
     alignSelf: "center",
     display: "flex",
     justifyContent: "space-around",
     width: "150px",
 };
-export const footerSocialMediaStyle = {
+export const footerSocialMediaStyles = {
     color: BLEACHED_SILK_COLOR,
     alignSelf: "center",
 };
