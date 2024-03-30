2ef055174fd9fb9fc0a72235fe2950c20be3a4d7
mouhamad <mouhamadx92x@gmail.com>
2024-03-30 02:50:16 +0100

    Footer main box resize

--- src/app/components/footer/styles.tsx
+++ src/app/components/footer/styles.tsx
@@ -1,13 +1,19 @@
 import { BLEACHED_SILK_COLOR } from "../../../../public/constants/colors";
-
 export const footerMainBoxStyles = {
-    height: { xs: "600px", sm: "350px" },
+    display: "flex",
+    backgroundColor: "black",
+    justifyContent: "center",
+};
+
+export const footerBoxStyles = {
+    height: { xs: "580px", sm: "330px", md: "285px" },
     backgroundColor: "transparent",
     display: "flex",
     flexDirection: { xs: "column", sm: "row" },
     justifyContent: { xs: "space-between", md: "space-between" },
     alignItems: { xs: "center", md: "center" },
-    padding: { xs: "10px", md: "30px" },
-    width: { xs: "375px", sm: "900px" },
+    paddingTop: { xs: "5px", sm: "5px", md: "15px" },
+    paddingBottom: { xs: "5px", sm: "5px", md: "15px" },
+    width: { xs: "375px", sm: "500px", md: "900px" },
     color: BLEACHED_SILK_COLOR,
 };
